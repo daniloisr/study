5244e337a9c9087351881d89eb52f2a64f7c769d
Danilo Inacio <danilo.isr@gmail.com>
2010-09-09 23:47:21 -0300

    working...

--- flod-it.js
+++ flod-it.js
@@ -2,11 +2,17 @@ function Vector (x, y){
     this.x = x || 0;
     this.y = y || 0;
     this.size;
+    
+    this.toString = function(){
+        return "x: " + this.x + ", y: " + this.y;
+    }
 }
 
 function BlockGroup(nBlocks, bWidth) {
     this.nBlocks = nBlocks;
     this.blocks = [];
+    
+    var ml = 100; // margin-left
     
     for(i=0; i < nBlocks; i++){
         this.blocks[i] = [];
@@ -30,13 +36,22 @@ function BlockGroup(nBlocks, bWidth) {
     
     this.draw = function(){
         // improve this...
-        ctx.clearRect(0, 0, 500, 500);
+        ctx.clearRect(100, 0, 500, 500);
         for(var i=0; i<this.blocks.length; i++){
             for(var j=0; j<this.blocks[i].length; j++){
                 var b = this.blocks[i][j];
                 //log(b.toString());
                 //log(i*b.width +", "+ j*b.height +", "+ b.width +", "+ b.height +", "+ b.color);
-                dRect(ctx, i*b.size, j*b.size, b.size, b.size, b.color);
+                dRect(ctx, i*b.size+ml, j*b.size, b.size, b.size, b.color);
+            }
+        }
+    };
+    
+    this.suffle = function(){
+        for(var i=0; i<nBlocks; i++){
+            for(var j=0; j<nBlocks; j++){
+                var color = colors[Math.floor(Math.random()*colors.length)];
+                this.blocks[i][j].color = color;
             }
         }
     };
@@ -48,9 +63,13 @@ function BlockGroup(nBlocks, bWidth) {
             }
         }
     };
+    
+    // test
+    this.suffle();
 }
 
 function Block(pos, size, color){
+
     this.pos     = pos;
     this.size    = size;
     this.color   = color || "#fff";
@@ -68,26 +87,76 @@ function Block(pos, size, color){
         return result;
     }
     
+    this.getX = function(){ return this.pos.x; }
+    this.getY = function(){ return this.pos.y; }
+    
     this.toString = function(){
-        return this.size + ", " + this.color + ", " + this.width + ", " + this.height;
+        return this.size + ", " + this.color + ", " + this.width + ", " + this.height + ", " + this.pos.toString();
     }
 }
 
-function $ (el){ return document.getElementById(el); }
-function log (msg) { $("log").innerHTML = msg + "<br>" + $("log").innerHTML; }
+function Menu(colors){
+    this.menus = [];
+    this.over  = false;
+    
+    var size = 50;
+    var mt   = 25; //margin-top
+    var ml   = 15; //margin-left
+
+    for(i=0; i < colors.length; i++){
+        this.menus[i] = new Block(new Vector(ml, i*size+(mt*(i+1))), size, colors[i]);
+        this.menus[i].selected = false;
+    }
+    
+    this.draw = function() {
+        for(i in this.menus){
+            tp = this.menus[i];
+            if(!tp.selected){
+                dRect(ctx, tp.getX(), tp.getY(), tp.size, tp.size, tp.color);
+            }
+            else{
+                ctx.save();
+                ctx.fillStyle = "#000";
+                ctx.lineWidth = 3;
+                ctx.strokeRect(tp.getX()+2, tp.getY()+2, tp.size-4, tp.size-4);
+                ctx.restore();
+                
+            }
+        }
+    }
+    
+    this.isOver = function(mouseCoords){
+        
+        x = mouseCoords.x;
+        y = mouseCoords.y;
+        for(var i=0; i<this.menus.length; i++){
+
+            if(x > this.menus[i].getX() && x < this.menus[i].getX() + this.menus[i].size &&
+                y > this.menus[i].getY() && y < this.menus[i].getY() + this.menus[i].size){
+                
+                this.menus[i].selected = true;
+                if(!this.over) this.draw();
+                
+                this.over = true;
+                return this.menus[i];
+            }
+            this.menus[i].selected = false;
+        }
+        if(this.over) this.draw();
+        this.over = false;
+    }
+}
 
 function dRect(ctx, x, y, w, h, color){
     ctx.fillStyle = color;
     ctx.fillRect(x, y, w, h);
 }
 
-var blocks_array = [];
-var blocks_painted = [];
-var menu = [];
-var nBlocks = 10;
-var block_width = 50;
+var menu;
+var nBlocks = 14;
+var block_width = Math.ceil(500/14);
 var ctx;
-var colors = ['#33FF00', '#6600FF', '#FFFF00', '#FF0000', '#66FFFF'];
+var colors = ['#33FF00', '#6600FF', '#FFFF00', '#FF0000', '#66FFFF', "#FF00FF"];
 var bGroup;
 
 window.onload = function() {
@@ -95,52 +164,19 @@ window.onload = function() {
     ctx = $("canvas").getContext("2d");
 
     // improve
-    bGroup = new BlockGroup(nBlocks, 50);
+    bGroup = new BlockGroup(nBlocks, block_width);
+    bGroup.suffle();
     
-    for(var i=0; i<nBlocks; i++){
-        // remove
-        blocks_array[i] = [];
-        blocks_painted[i] = [];
-        
-        for(var j=0; j<nBlocks; j++){
-            var color = colors[Math.floor(Math.random()*colors.length)];
-            bGroup.get(i,j).color = color;
-            
-            // remove
-            blocks_array[i][j] = color;
-            blocks_painted[i][j] = false;
-        }
-    }
-
-    // menu builder, move this to a function
-    for(var i=0; i<colors.length; i++){
-        menu[i] = new Vector();
-        menu[i].x = 525;
-        menu[i].y = (i*50)+((1+i)*10);
-        menu[i].size = block_width;
-
-        dRect(ctx, 525, menu[i].y, block_width, block_width, colors[i]);
-    }
+    menu = new Menu(colors);
+    menu.draw();
 
     bGroup.draw();
 };
 
-/*/ remove
-function draw(){
-    ctx.clearRect(0, 0, 500, 500);
-    for(var i=0; i<nBlocks; i++){
-        for(var j=0; j<nBlocks; j++){
-            dRect(ctx, i*block_width, j*block_width, block_width, block_width, blocks_array[i][j]);
-        }
-    }
-}
-*/
-
-function getClickCoord(e){
+function getMouseCoords(e){
     var c_offset = canvasPosition(); 
     
     var result = new Vector(e.pageX - c_offset.x, e.pageY - c_offset.y);
-    //var result = new Vector(e.pageX, e.pageY);
     return result;
 }
 
@@ -150,38 +186,25 @@ function canvasPosition(){
 
 document.onmousedown = function(e){
     // improve this
-    var click_coord = getClickCoord(e);
+    var click_coord = getMouseCoords(e);
     var selected;
 
-    for(var i=0; i<menu.length; i++){
-        x = click_coord.x;
-        y = click_coord.y;
-
-        if(x > menu[i].x && x < menu[i].x + menu[i].size &&
-            y > menu[i].y && y < menu[i].y + menu[i].size){
-
-            selected = colors[i];
-            break;
-        }
-    }
+    selected = menu.isOver(click_coord);
 
     if(selected != null){
-        flodit(selected);
-        log(selected);
+        flodit(selected.color);
     }
+}
+
+document.onmousemove = function (event) {
+    var mouseCoords = getMouseCoords(event);
+    
+    menu.isOver(mouseCoords);
 }
 
 function flodit(newColor){
     // resta os blocos pintados
     bGroup.reset();
-    /*/ remove
-    for(var i=0; i<blocks_painted.length; i++){
-        for(var j=0; j<blocks_painted.length; j++){
-            blocks_painted[i][j] = false;
-        }
-    }
-    blocks_painted[0][0] = true;
-    */
 
     var bCurrent;
     var bStack = [bGroup.get(0, 0)];
@@ -204,54 +227,7 @@ function flodit(newColor){
     
     bGroup.draw();
     
-    /*
-    var cor = blocks_array[0][0];
-    var same_color = [new Vector(0, 0)];
-    var flooded = [];
-
-    for(var b=0; b<same_color.length; b++) {
-            // pega vizinhos
-            x = same_color[b].x;
-            y = same_color[b].y;
-
-            if(x>0)
-                if(blocks_array[x-1][y] == cor && !blocks_painted[x-1][y]){
-                    same_color.push(new Vector(x-1, y));
-                    blocks_painted[x-1][y] = true;
-                }
-                else if(blocks_array[x-1][y] == newColor)
-                    flooded.push(new Vector(x-1, y));
-
-            if(x<blocks_array.length-1)
-                if(blocks_array[x+1][y] == cor && !blocks_painted[x+1][y]){
-                    same_color.push(new Vector(x+1, y));
-                    blocks_painted[x+1][y] = true;
-                }
-                else if(blocks_array[x+1][y] == newColor)
-                    flooded.push(new Vector(x+1, y));
-
-            if(y>0)
-                if(blocks_array[x][y-1] == cor && !blocks_painted[x][y-1]){
-                    same_color.push(new Vector(x, y-1));
-                    blocks_painted[x][y-1] = true;
-                }
-                else if(blocks_array[x][y-1] == newColor)
-                    flooded.push(new Vector(x, y-1));
-
-            if(y<blocks_array[x].length-1)
-                if(blocks_array[x][y+1] == cor && !blocks_painted[x][y+1]){
-                    same_color.push(new Vector(x, y+1));
-                    blocks_painted[x][y+1] = true;
-                }
-                else if(blocks_array[x][y+1] == newColor)
-                    flooded.push(new Vector(x, y+1));
-
-            blocks_array[x][y] = newColor;
-    }
-    for(b in flooded){
-        blocks_array[flooded[b].x][flooded[b].y] = newColor;
-    }
-
-    draw();
-    */
 }
+
+function $ (el){ return document.getElementById(el); }
+function log (msg) { $("log").innerHTML = msg + "<br>" + $("log").innerHTML; }
