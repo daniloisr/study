0c15b47322275192b0a3638d220b1e727999d754
Danilo Resende <danilo.isr@gmail.com>
2015-12-06 22:47:38 -0200

    wip: add grid
    Former-commit-id: ec64aa46a55168ea77fe93c4942d71a283cd4d74 [formerly 8c628cc2885e8528a1dfe31c0ab18f52da628e75] [formerly d237d720fe06f1fdb0ec83d0cf8b13dc8560a721] [formerly 360edecf0cf76cd836f3e38e81e0a69dca0e87c4 [formerly bc9b31e190ed295d26ffa4ef45a54cd910e66dc5]]
    Former-commit-id: d444f4683756bbb158dd11f67ff2ee09e7bbb633
    Former-commit-id: 270f5dc4cc80be0effeb7139d9dd3cc423bd1dc5
    Former-commit-id: 814ffb0328a34ecde0c5b0487275494c76354ece
    Former-commit-id: e5e82e5d177c976d4ca5fda6d2e062ad860e328b
    Former-commit-id: 7da382baedad09280f102e1e3e8db0450bb41065
    Former-commit-id: d8494223c1c5c848309cd92a35916de28b9ed261

--- elm/playground/lights-out/LightsOut.elm
+++ elm/playground/lights-out/LightsOut.elm
@@ -4,27 +4,36 @@ import Graphics.Element exposing (..)
 import List exposing (repeat, map)
 
 type alias Model =
-  { s : Int
+  { s : Float
+  , p : Float
   , r : Int
   , c : Int
   }
 
 game : Model
 game =
-  { s = 20
-  , r = 2
-  , c = 2
+  { s = 30
+  , p = 3
+  , r = 6
+  , c = 6
   }
 
 main : Element
 main =
   collage 400 400
-    (map
-      (\x -> rect 20 20
-        |> filled alphaBlue
-        |> move (x*10,-10))
-      [0..3])
+            (map
+             (\count ->
+                let
+                  distance = game.s + game.p
+                  x = toFloat(count // game.r) * distance
+                  y = toFloat(count % game.r) * distance
+                in
+                  rect game.s game.s
+                    |> filled alphaBlue
+                    |> move (x - 180, y + 15)
+             )
+             [0..((game.r * game.c) - 1)])
 
 alphaBlue : Color
 alphaBlue =
-  rgba 100 100 200 0.1
+  rgb 100 100 200
