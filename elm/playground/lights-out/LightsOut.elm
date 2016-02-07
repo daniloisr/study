7db50f3f6ed18efbff86d134ffbe0466801553a3
Danilo inácio <danilo.isr@gmail.com>
2016-02-07 21:34:37 +0000

    working grid
    Former-commit-id: aa86f16c01d9e37c7bfc35b52aa3ea19f7f1e748 [formerly f0e105688c8f3a0f7e2518e650d5c132cd3ff0b1] [formerly 03ab5951cc3606a1d7d767831f4c3b0b279020f1] [formerly c09513beec3d7925319c2da0b035af6d66f951db [formerly 54635e62df0d1da518742ff40018a58d4ac72bd4]]
    Former-commit-id: d72f670512ec2328c70924e965eb5700126c18cb
    Former-commit-id: d5a6aaefa3bc859e4f54d7b7118562d4690e62aa
    Former-commit-id: d2c4350b3a902ae3da085614d224baadf5b603de
    Former-commit-id: 3b2b3a5dcfadf0a530ca2094b81fe9609ef5b753
    Former-commit-id: be636991fec5870f0b16190d593447bfd9f4b95d
    Former-commit-id: 757e80c8e9aafd1b6adb06b3d66a33889889ea32

--- elm/playground/lights-out/LightsOut.elm
+++ elm/playground/lights-out/LightsOut.elm
@@ -1,92 +1,53 @@
-import Color exposing (..)
+import Color
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import List
 import Mouse
-import Text
 import Color
 import Window
 
-type alias Model =
-  { s : Float -- size
-  , p : Float -- padding
-  , r : Int   -- rows
-  , c : Int   -- coluns
-  }
-
-game : Model
-game =
-  { s = 30
-  , p = 3
-  , r = 6
-  , c = 6
-  }
-
-main1 : Element
-main1 =
-  collage 400 400
-            (List.map
-             (\count ->
-                let
-                  distance = game.s + game.p
-                  x = toFloat(count // game.r) * distance
-                  y = toFloat(count % game.r) * distance
-                in
-                  rect game.s game.s
-                    |> filled alphaBlue
-                    |> move (x - 180, y + 15)
-             )
-             [0..((game.r * game.c) - 1)])
-
-alphaBlue : Color
-alphaBlue =
-  rgb 100 100 200
-
-
-
-
-view : (Int, Int) -> (Int, Int) -> Element
-view (x, y) (w, h) =
-  collage w h
-    [ drawSquare (x - w//2, h//2 - y) (List.head model)
-    , drawSquare (x - w//2, h//2 - y) (List.head (List.reverse model))
-    , Text.fromString (toString (x - w//2, h//2 - y))
-      |> leftAligned
-      |> toForm
-    ]
-
-type state = ON | OFF
-
-drawSquare : (Int, Int) -> Maybe Square -> Form
-drawSquare (x, y) s =
-  case s of
-    Just s ->
-      let
-        x1 = (round s.x) - (round (s.size/2))
-        y1 = (round s.y) - (round (s.size/2))
-        x2 = (round s.x) + (round (s.size/2))
-        y2 = (round s.y) + (round (s.size/2))
-        color = if x >= x1 && x <= x2 && y >= y1 && y <= y2 then Color.red else Color.blue
-      in
-        square s.size |> filled color |> move (s.x, s.y)
-    Nothing ->
-      toForm (spacer 0 0)
+size = 50
+padding = 2
+num = 3
 
 main : Signal Element
 main =
   Signal.map2 view (Signal.sampleOn Mouse.clicks Mouse.position) Window.dimensions
 
+type State = ON | OFF
 
+model : List State
+model =
+  List.map (always OFF) [1..(num * num)]
 
+view : (Int, Int) -> (Int, Int) -> Element
+view (x, y) (w, h) =
+  let
+    x' = x - w//2 + (size // 2)
+    y' = y - h//2 - (size // 2)
+  in
+    collage w h
+      (List.append
+        (List.indexedMap (drawSquare (x', -y')) model)
+        [toForm <| show (x', -y')])
 
--- mainClick : Signal Element
--- mainClick =
---   Signal.map show countClick
+drawSquare : (Int, Int) -> Int -> State -> Form
+drawSquare (mx, my) index state =
+  let
+    color = if (clicked (mx, my) index) then Color.red else Color.blue
+    x = (index % num) * size
+    y = (index // num) * size
+  in
+    square (size - (padding * 2)) |> filled color |> move (toFloat x, toFloat y)
 
--- countClick : Signal (Int, Int)
--- countClick =
---   Signal.foldp
---     (\(x, y) (mx, my) -> (x + mx, y + my))
---     (0, 0)
---     (Signal.sampleOn Mouse.clicks Mouse.position)
-    
+clicked : (Int, Int) -> Int -> Bool
+clicked (mx, my) index =
+  let
+    ix = index % num
+    iy = index // num
+    (mx', my') = (mx % size, my % size)
+    b1 = mx // size == ix && my // size == iy
+    b2 = mx' - padding > 0 && my' - padding > 0
+    b3 = mx' + padding < size && my' + padding < size
+  in
+    b1 && b2 && b3
