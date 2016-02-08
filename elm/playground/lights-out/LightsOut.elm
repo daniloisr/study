96e8c71662fa84051303c5c3f32467c72a171aee
Danilo inácio <danilo.isr@gmail.com>
2016-02-08 22:18:19 +0000

    Part 1 finished
    Former-commit-id: 26674ee4af834355acb440ad9ef7cf0f07bfd2af [formerly 4330998ed41668964692b335774b413ae137e53d] [formerly 2f95f154f523a994b9712b65da6c9260660603e8] [formerly cc6db22cdff38e23cef2579758c2b4ca3f61050b [formerly 16ba7a0523777800e49831be79ae8efb20501280]]
    Former-commit-id: eff9ada853311237fa8c6e11a283c3d1cb1327d6
    Former-commit-id: df1c3a9f19720c7bbf4493a021ed15d7c76f62ad
    Former-commit-id: 245910a34aa956cb021521f8c6e2f4fee063c23c
    Former-commit-id: 7fa17f606092c6a5b0641cf9d8bfb1242ae81794
    Former-commit-id: 0d0da9d679511c98adcff348bf4c522fd59742cd
    Former-commit-id: f3958f427e8c318dd6ed3401046a10eecf88dbf5

--- elm/playground/lights-out/LightsOut.elm
+++ elm/playground/lights-out/LightsOut.elm
@@ -7,28 +7,7 @@ import Window
 
 size = 50
 padding = 2
-num = 3
-
-main : Signal Element
-main =
-  Signal.map2 view Window.dimensions gameState
-
-gameState : Signal Game
-gameState =
-  Signal.foldp update newGame (Signal.sampleOn Mouse.clicks Mouse.position)
-
-update : (Int, Int) -> Game -> Game
-update (x, y) game =
-  let
-    x = 1
-    -- x' = x - w//2 + (size // 2)
-    -- y' = y - h//2 - (size // 2)
-  in
-    -- collage w h
-    --   (List.append
-    --     (List.indexedMap (drawSquare (x', -y')) game.ligths)
-    --     [toForm <| show (x', -y')])
-    game
+num = 5
 
 type alias Game =
   { lights : List Bool }
@@ -37,6 +16,66 @@ newGame : Game
 newGame =
   { lights = List.map (always False) [1..(num * num)] }
 
+type alias Input =
+  { dimension : (Int, Int)
+  , click : (Int, Int)
+  }
+
+main : Signal Element
+main =
+  Signal.map2 view Window.dimensions gameState
+
+gameState : Signal Game
+gameState =
+  Signal.foldp update newGame input
+
+input : Signal Input
+input =
+  Signal.sampleOn Mouse.clicks
+    <| Signal.map2 Input Window.dimensions (Signal.sampleOn Mouse.clicks Mouse.position)
+
+
+update : Input -> Game -> Game
+update {dimension, click} game =
+  let
+    (w, h) = dimension
+    (x, y) = click
+    x' = x - w//2 + (size // 2)
+    y' = -(y - h//2 - (size // 2))
+    target = clicked (x',y')
+    newLights =
+      if target > -1 then
+        List.indexedMap (toggle target) game.lights
+      else
+        game.lights
+  in
+    { game |
+      lights = newLights
+    }
+
+clicked : (Int, Int) -> Int
+clicked (x, y) =
+  let
+    xindex = x // size
+    yindex = y // size
+  in
+    if x <= 0 || y <= 0 || x > num * size || y > num * size then
+      -1
+    else
+      xindex + (yindex * num)
+
+toggle : Int -> Int -> Bool -> Bool
+toggle target index value =
+  if ((index == 0 || index % num /= num - 1) && index == target - 1) ||
+    ((index == (num * num) - 1 || index % num /= 0) && index == target + 1) ||
+    index == target + num ||
+    index == target - num ||
+    index == target then
+    not value
+  else
+    value
+
+
 view : (Int, Int) -> Game -> Element
 view (w, h) game =
   collage w h (List.indexedMap drawSquare game.lights)
@@ -44,20 +83,10 @@ view (w, h) game =
 drawSquare : Int -> Bool -> Form
 drawSquare index state =
   let
-    color = if False then Color.red else Color.blue
+    color = if state then Color.red else Color.blue
     x = (index % num) * size
     y = (index // num) * size
   in
-    square (size - (padding * 2)) |> filled color |> move (toFloat x, toFloat y)
-
-clicked : (Int, Int) -> Int -> Bool
-clicked (mx, my) index =
-  let
-    ix = index % num
-    iy = index // num
-    (mx', my') = (mx % size, my % size)
-    b1 = mx // size == ix && my // size == iy
-    b2 = mx' - padding > 0 && my' - padding > 0
-    b3 = mx' + padding < size && my' + padding < size
-  in
-    mx > 0 && my > 0 && b1 && b2 && b3
+    square (size - (padding * 2))
+      |> filled color
+      |> move (toFloat x, toFloat y)
