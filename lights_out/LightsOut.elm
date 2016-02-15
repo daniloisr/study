878562ec2ae4dc873314bb0ae526e8c9a2d515ed
Danilo inácio <danilo.isr@gmail.com>
2016-02-15 21:09:14 +0000

    Add hovering effect
    Former-commit-id: 2a098967aff102b03a2eb1488561bfa4803b3380
    Former-commit-id: 067c969b8d73462867bd8261e500e53eb9ec4159 [formerly 8932b14b2de2c6346e39ded41ae16c18c6ff6e57] [formerly 446c756f2c6881ba0e046bd4d8e1f830f8fe4679 [formerly c636122c6210a57cf06ee7b7c4f15df181ffec8b]] [formerly 19abe605a46c1da585c4b469837d8d3c48173d44] [formerly ef46cbf26e41d6a473f61e8f291aa2ce016c0693] [formerly 89119719fb60dbb5d382244c68d449d739b0506c] [formerly 85a2ce585b4bb85a165dc4a64ca221a79e05f1cf] [formerly bdf54fbd05d0b5ccf9a8640e3cf28a2ddae21dd8 [formerly 431e4de370c9947e156d21b495e03597e215144c] [formerly 3e73bda2edd5b3ca5971ce7046b5cd4a669fe1db] [formerly 7bc5621a3352aa863e078f2fe4e7d1a466030601] [formerly 4545783ba591f75d7a4d605988f1b8e67fe4eb1e] [formerly dba8fffa3da1e8de51e6804d234369d39d0fe170] [formerly 6438d3b219f5616cfdf315c435aae4469e60f786] [formerly 95377ed558da2cabd0ca10fc27c67360f2d8ca94] [formerly 510c4c4400d75ff0fa1a7d7d7b8c0c256f89204c] [formerly 410487bf6c7f6e818abb890ecbe08d742195ba62] [formerly 6e5982c10dcc785c30b5cd8f29b9425ba810db32 [formerly e896aa166d49ec6d40f075412a1e0f12052b8509]]]
    Former-commit-id: 067c969b8d73462867bd8261e500e53eb9ec4159 [formerly 8932b14b2de2c6346e39ded41ae16c18c6ff6e57] [formerly 446c756f2c6881ba0e046bd4d8e1f830f8fe4679 [formerly c636122c6210a57cf06ee7b7c4f15df181ffec8b]] [formerly 19abe605a46c1da585c4b469837d8d3c48173d44] [formerly ef46cbf26e41d6a473f61e8f291aa2ce016c0693] [formerly 89119719fb60dbb5d382244c68d449d739b0506c] [formerly 85a2ce585b4bb85a165dc4a64ca221a79e05f1cf]
    Former-commit-id: 225ba01664f5b1eee08ee0c95fd37fc33ff40215
    Former-commit-id: 6b6a342c55650b0c4802604e598d66c0afa7d22e
    Former-commit-id: ced77ab5d632f6bf8a995b06d526bc70782fc699
    Former-commit-id: 04b94ed4c9e49733bfc4854600caa76132edc5ae
    Former-commit-id: 197310aef5619f4411685dc4132bf39e7d5aa5f3
    Former-commit-id: 72c9368bfdcef4021c31df90eb0eabdc847e2090
    Former-commit-id: 7cc3ee630ac0d9b469cabbd57fa2c260748638a3
    Former-commit-id: 28e3da6cef15faac0010b63f0afb591c7fd12b3c
    Former-commit-id: fbf1d3762c1e177fbdf7574eed902b0873aa731b
    Former-commit-id: 22ff9bc4144276e8528f13944181e6b49af8d1bd [formerly 2dc4fa09d3849c51ac5e0acde8a227051f343dcb]
    Former-commit-id: 9f4f790f0d07a1d2558fd507838670c8e7eb77e4
    Former-commit-id: bc3911f7e8dfaa0928fa7ce34835182c6fa2af3b
    Former-commit-id: 8d4e268cc5f62c7f2acfab76417c9fe1f8e9b57e
    Former-commit-id: 17e39ea14cd4a8ea4740f9650ecfdc9f78007826
    Former-commit-id: 431ca251fc7b3646b7e979dbf6c9a3f509dfcbb9
    Former-commit-id: 85de48627b6368aa372e2aad127d1c1ed7e82f3d
    Former-commit-id: e7164468b253705778bc25d929e196e02b55b1f3
    Former-commit-id: 2ee79e9154d47571c751a6bb316e22cdad706890
    Former-commit-id: 21da2291728439feb7071a68a89ca0a9e23a58ff
    Former-commit-id: 20563c847e360c0ac2f79186fb85a813453f85ce [formerly edecb7a8b5b831f31f88bd3a3cad37c350fa3516] [formerly 276eaa25c1f5f612370eaafa2ea1b02c7b97b76f [formerly 23575e33be75d85d175ed28984f47d8ed7eafec1]] [formerly 4a3468a863adafb47cf3b310883d33a9b873b2aa [formerly dbd14b851ec1c72e2ee8e0e76fdcdea5360e2075] [formerly dc0e40c8bb5c2701bfa9e97ccd950182a3fcf202 [formerly 6377cbc3d85f6e69cea49a3eda42401adebd9a9c]]]
    Former-commit-id: d0d48af08702cf78f1159a347d6a113ad0640a30 [formerly f5a04fcba425251caeb99963d35d3e35d58723f6] [formerly 19f8a932846150b427fd3a7b6946826b243418aa [formerly ce3bf892cac77ea2e39bb909c0cad43726648803]]
    Former-commit-id: b9b807d2f7f1b4b2d63c9c61228c98b4b9bd46f0 [formerly 913a2635a2750a515a8aedfdec12eed3b62af5c4]
    Former-commit-id: 7d41c1565feab4b0ed2c1ea2deb97ca2001639fc

--- lights_out/LightsOut.elm
+++ lights_out/LightsOut.elm
@@ -1,9 +1,9 @@
 -- for index query
 -- http://programmers.stackexchange.com/a/131705/50321
-import Graphics.Collage exposing (Form, collage, square, filled, move)
-import Graphics.Element exposing (Element)
-import Color exposing (gray, black)
-import List exposing (map, map2, repeat, concat)
+import Graphics.Collage exposing (Form, collage, square, filled, move, toForm)
+import Graphics.Element exposing (Element, show)
+import Color exposing (red, rgba)
+import List exposing (map, map2, repeat, concat, (::))
 import Signal
 import Mouse
 import Time
@@ -11,50 +11,85 @@ import Time
 type alias Point =
   { x: Int
   , y: Int
+  , hover: Float
+  , clicked: Bool
   }
 
 p : Int -> Int -> Point
 p x y =
   { x = x
   , y = y
+  , hover = 0
+  , clicked = False
   }
 
 type alias Model =
   { points: List Point
-  , hover: List Float
-  , clicked: List Bool
+  , input: Input
   }
 
 type alias Input =
   { mousep: (Int,Int)
   , delta: Time.Time
+  , clicked: Bool
   }
 
 initialModel : Model
 initialModel =
   let
-    xs = concat <| map (repeat 3) [0..2]
-    ys = concat <| repeat 3 [0..2]
+    xs = concat <| map (repeat 5) [0..4]
+    ys = concat <| repeat 5 [0..4]
   in
     { points = map2 p xs ys
-    , hover = repeat 9 0
-    , clicked = repeat 9 False
+    , input = { mousep = (0, 0), delta = 0, clicked = False }
     }
 
 paintSquare : Point -> Form
 paintSquare point =
-  square 20 |> filled gray |> move ((toFloat point.x * 22), (toFloat point.y * 22))
+  square 50 |>
+    filled (if point.clicked then red else (rgba 0 0 0 point.hover)) |>
+    move (toFloat point.x * 52, toFloat point.y * 52)
 
 main : Signal Element
 main =
   Signal.map
-    (\x -> collage 300 300 (map paintSquare initialModel.points))
+    (\model ->
+      map paintSquare model.points
+        |> (::) (show model.input |> toForm |> move (-20, -100))
+        |> collage 500 500
+    )
     currentModel
 
 currentModel : Signal Model
 currentModel =
-  Signal.foldp (flip always) initialModel input
+  Signal.foldp update initialModel input
 
 input : Signal Input
 input =
-  Signal.map2 Input Mouse.position (Time.fps 50 |> Signal.map Time.inSeconds)
+  Signal.map3 Input
+    Mouse.position
+    (Time.fps 1 |> Signal.map Time.inSeconds)
+    (Signal.dropRepeats Mouse.isDown)
+
+update : Input -> Model -> Model
+update {mousep, delta, clicked} model =
+  let
+    (x', y') = mousep
+    (x, y) = (x' - 225, 275 - y')
+    px = x // 50
+    py = y // 50
+    points = map (\p ->
+        if p.x == px && p.y == py then
+          { p |
+            hover = 1,
+            clicked = if clicked then not p.clicked else p.clicked }
+        else
+          { p |
+            hover = max (p.hover - 0.1) 0.3 }
+      )
+      model.points
+  in
+    { model |
+      input = { mousep = (x, y), delta = 0, clicked = clicked },
+      points = points
+    }
