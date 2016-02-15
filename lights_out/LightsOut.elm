519d146fb193105b62e64878e642c224a873f45d
Danilo inácio <danilo.isr@gmail.com>
2016-02-15 15:31:16 +0000

    Adding mouse movements
    Former-commit-id: f06bb0f67f28e9fde28df9ea795874825abea881
    Former-commit-id: 5b6c540ce6fb66216d6117bf051de766535bfbad [formerly dd31d345a36f5f09c9d02180b0920125591c9279] [formerly 35b2e2c935d7bae4bc72b9ab3af4f99400bfc0e5 [formerly 6ef2e84e8556b48ded2d57e32d269357370bb370]] [formerly 8a6e9ee7fbf0748ec3adf421a9eeb7e455cd6c03] [formerly 63c9c398239ce667a18b69e94098cf59b7abd9c8] [formerly 64c491257855e8660c076669d95544aa798a31be] [formerly 787fd2815a1841d32eecc6e0ac8a0a321e0ad0fa] [formerly 08c0ae7b47f980c6d4742bca982ba39e9c800a05 [formerly f0e3a773043f349a53c80afc263d6afbb91bf53b] [formerly 1eb231a6f81f78b63f45d8850ee3e6f3b2419e1a] [formerly 0a492f120952daa959e7ca282748d50365cd7b05] [formerly d95d85ae8138f3905b8e5a8f73f0e4123f41cc24] [formerly ad2c99c165a47360f01d2c4fb1a479086712dfb6] [formerly 38be72c06bd76f8bebbe5c3b0e5221423b4d009e] [formerly 9b942e258fc58ae87f4ddf507a7b62f10ec6d9b3] [formerly bfd052f2f2a47740699ea368497e091163af289b] [formerly 2cd08f007a6d9ced81f48c6de5ee2682c82de938] [formerly 73b92641dc259d1d75d3d7dbfc5f48286de85013 [formerly 460a86039a252213f4a366eaeca3288ac1483ddf]]]
    Former-commit-id: 5b6c540ce6fb66216d6117bf051de766535bfbad [formerly dd31d345a36f5f09c9d02180b0920125591c9279] [formerly 35b2e2c935d7bae4bc72b9ab3af4f99400bfc0e5 [formerly 6ef2e84e8556b48ded2d57e32d269357370bb370]] [formerly 8a6e9ee7fbf0748ec3adf421a9eeb7e455cd6c03] [formerly 63c9c398239ce667a18b69e94098cf59b7abd9c8] [formerly 64c491257855e8660c076669d95544aa798a31be] [formerly 787fd2815a1841d32eecc6e0ac8a0a321e0ad0fa]
    Former-commit-id: bea671e0abab3c39d8e7cd3a2f0271579842888c
    Former-commit-id: 0f35c9788938cb6dda827693a61a5379311bce24
    Former-commit-id: 9e31dd134c6dafd7f50a5f3b5fc969629d3dc1b1
    Former-commit-id: 7b1fe616fcbecdedf42e74f134c94a3450f895f1
    Former-commit-id: f98f5718e2267725c577cef44a1cdca8059fb059
    Former-commit-id: 585ff0cc1f236520d14e682bf7c2683e2f06f39b
    Former-commit-id: 0d8c5bdde24b37c47d6a07f306e90aea0fa8bc60
    Former-commit-id: 389323fd9e12eef11f0b6694e66acf409bdb8dfc
    Former-commit-id: 314a6217ac851284f8d88d785547cb7daf1e01c3
    Former-commit-id: b1c9c2d553a3a664c3f1ed9d393b02075b4dc0dd [formerly 4825664394adadd1aea0a409c8d1246e0a59d94b]
    Former-commit-id: cb9e7a8bfc9dbaf2cb0eab9891bc9f8d2c9fe0b1
    Former-commit-id: 45aaa3e9493315241556e7d8a5416c6a292c204d
    Former-commit-id: ee9f3057e40c5aaefe86c70f71e2b4b724442c78
    Former-commit-id: 3e44c4f5cdd2df0cae6bc4a047b43175f170cd18
    Former-commit-id: 156b4ecfe70bceace7e793a3924810c5ddb46e39
    Former-commit-id: 6e8b6a64f2e2f14e4e89863393d36c64f0c3ed84
    Former-commit-id: e399b931c71608378b05b3c922c94c7bf5dbd005
    Former-commit-id: a0e7c1a5038c5473c6e6e3f8f4e8e840146250c6
    Former-commit-id: f88ca48be4d7837c241d4105ddb5c35f5977617a
    Former-commit-id: e8d532b94664b05ad8071f07efd8002524940b14 [formerly d02db9818ace8be0092d053d26f8abad100f5602] [formerly 8203c618d8ef94387e2cc1f9671cfb1400794cfb [formerly fa543b796988083ace185b66b8ccb42b01d9021e]] [formerly ec023f22b8c377d7276ebdf070831650ac895af2 [formerly eecac1ab0ffdda6fcecb0884ae2ed8db029d4d3a] [formerly 736bc1f27ee0979d5a8238c768322ab10a3d8d83 [formerly 1ac952b027d806111e268d7975b2812624443184]]]
    Former-commit-id: 5f3dd4e5905fe70e1ed2a65beb250e0b59785e35 [formerly a21be1094194c578d1dfdb85578dc7a981d023df] [formerly f5c4a4f526bf9427031679ac4dad880716ce225e [formerly fe66e31af54e88820cab74b23f3cae23c2642563]]
    Former-commit-id: 2eeaf286fe8291f7e1ef967b74c9ae9dbf9dc2dc [formerly e64f7f9e9d551bdddc55786b7b232915bab507a0]
    Former-commit-id: afe3279520424816ba6dd8b3a74d36209c72eca3

--- lights_out/LightsOut.elm
+++ lights_out/LightsOut.elm
@@ -1,7 +1,12 @@
+-- for index query
+-- http://programmers.stackexchange.com/a/131705/50321
 import Graphics.Collage exposing (Form, collage, square, filled, move)
 import Graphics.Element exposing (Element)
 import Color exposing (gray, black)
 import List exposing (map, map2, repeat, concat)
+import Signal
+import Mouse
+import Time
 
 type alias Point =
   { x: Int
@@ -14,20 +19,42 @@ p x y =
   , y = y
   }
 
+type alias Model =
+  { points: List Point
+  , hover: List Float
+  , clicked: List Bool
+  }
 
-points : List Point
-points =
+type alias Input =
+  { mousep: (Int,Int)
+  , delta: Time.Time
+  }
+
+initialModel : Model
+initialModel =
   let
     xs = concat <| map (repeat 3) [0..2]
     ys = concat <| repeat 3 [0..2]
   in
-    map2 p xs ys
+    { points = map2 p xs ys
+    , hover = repeat 9 0
+    , clicked = repeat 9 False
+    }
 
 paintSquare : Point -> Form
 paintSquare point =
   square 20 |> filled gray |> move ((toFloat point.x * 22), (toFloat point.y * 22))
 
-main : Element
+main : Signal Element
 main =
-  collage 300 300
-    (map paintSquare points)
+  Signal.map
+    (\x -> collage 300 300 (map paintSquare initialModel.points))
+    currentModel
+
+currentModel : Signal Model
+currentModel =
+  Signal.foldp (flip always) initialModel input
+
+input : Signal Input
+input =
+  Signal.map2 Input Mouse.position (Time.fps 50 |> Signal.map Time.inSeconds)
