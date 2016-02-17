931d164903ad28190fa26474ac2fe2e3721133c0
Danilo inácio <danilo.isr@gmail.com>
2016-02-17 11:05:38 +0000

    feat: add reset button
    Former-commit-id: b45fb96272854b96ef3895e63703a13824d7bf88
    Former-commit-id: e85857d5d2400abad14e347164a537c3f1afc4d2 [formerly 52d04348194e2f0dedd5e3fd7a8a068e0653ea77] [formerly 39c6b18c8e3b29a2fda93ccb822fb5e57e8290fa [formerly 1d1e8fa1bdf8c32f026c77a62984a8d86a5c4b03]] [formerly fd19943c1f5812758c72fbc1cae70a08bdf341a8] [formerly 08eb780ec3770ce53a794423f24860ce61661c58] [formerly 235b84fc681c148ee41efda4d1e1b32169ad4271] [formerly 0382323b0143d97d1b9d592c77bfea44bf44b8ca] [formerly abd1f1e4440dbfbce18408828e0dca5fb71af4a6 [formerly 60545b5de033c12c8f21a69966a6d8b0208064cb] [formerly e752f26886f490b9838ab7740aa79b43bbeeb71d] [formerly 6beb298c5181861b042938c72589a9d76b549e43] [formerly 1387ec2329d210e72b2225bbac7a0752835ba000] [formerly 3db57f82380892fae40ed62e655e0db393e4fe98] [formerly a6efeff92a11e95416eb5d4dd387da73247a0c23] [formerly 040ede8cdef5d976082b70f04d4ed68bfd97bdd2] [formerly 8b5bc05a7c1c05d8814424d971964881cf4616cb] [formerly 1dcfa223ab5b1cfd896b10d0cfb32d33d8689265] [formerly 8d21b4250b0a1bfe3855c83f8c89be366c12bf77 [formerly ecef8280dc35d8eccd2896dc02e261216c200a87]]]
    Former-commit-id: e85857d5d2400abad14e347164a537c3f1afc4d2 [formerly 52d04348194e2f0dedd5e3fd7a8a068e0653ea77] [formerly 39c6b18c8e3b29a2fda93ccb822fb5e57e8290fa [formerly 1d1e8fa1bdf8c32f026c77a62984a8d86a5c4b03]] [formerly fd19943c1f5812758c72fbc1cae70a08bdf341a8] [formerly 08eb780ec3770ce53a794423f24860ce61661c58] [formerly 235b84fc681c148ee41efda4d1e1b32169ad4271] [formerly 0382323b0143d97d1b9d592c77bfea44bf44b8ca]
    Former-commit-id: f6215a97bceaa43bfa2b3e8bfcd193ee0ab906ff
    Former-commit-id: b70a9895ec0be2aae13d7a1928f21fcf419439ea
    Former-commit-id: 7f6a8397df258336dbe646b0abcb7435018896df
    Former-commit-id: 006d156c06962a00c51902af99eeed2b1d3c7c4b
    Former-commit-id: a57d7d6cb85d7771300a8b834d98f6eebe4a55b9
    Former-commit-id: bc1246ee34776d842627be4be023e954de81aae7
    Former-commit-id: c1b3dd98b81f9c724fb5236d01a45dcfe5c58901
    Former-commit-id: 84c928d4a2898ae7e5742a388423665bbaaef454
    Former-commit-id: 6d09d7a28a353530bd4481b8452a73fc76d39830
    Former-commit-id: bd2b2b53e1d11495689cc296185c27112f92a08b [formerly ab06d13794670cbc1b4d20647297a12698177c93]
    Former-commit-id: f3d9a1968a4b426290d9089ef4b1aea3009cc6d4
    Former-commit-id: 8101514833f9ff09bc0d120f7a81b47bbeddb346
    Former-commit-id: f092ea51db677cd1d871903c1bdbae051b517fa5
    Former-commit-id: e1c21d9184152fd75c911023332a6f24f151c4a8
    Former-commit-id: 554581818e9ad19fd99beae9f4b7dd5bfb82b404
    Former-commit-id: 7e9d6d12c888cac47e804e41a5b7435bd141e339
    Former-commit-id: 55b30ec4b972cab52d029f760a693418043e1928
    Former-commit-id: 287c9dac60c94aca187fb741b0069e0b36de15fa
    Former-commit-id: 718778273df193db3e76ce400138879e29dbb731
    Former-commit-id: f011c8b9f76ab2f5b08a14fae614e0a4ffbc1be3 [formerly b978fd4fe84924abe5e518053f7e7bc62ae7fe8e] [formerly dd8b153066deaa587c945115299e9d5adc18bacd [formerly c8455952608cacc537c8d68b09f7f4cebe3fc211]] [formerly 6b6b230b131c1e504c66f28438e69cf3a4ae6d0c [formerly 63260b375eb1d4784891ef0c13f574a45da65a38] [formerly b31f3c518532ee0aa85635a2d04ebaefbb2077eb [formerly bdd0e62ceebeb866c21c4acdcbbcb488a08ac716]]]
    Former-commit-id: 5a6544caf013ac2a48b525e63a5aaf20afd19b88 [formerly 952e773495bfbb9e0229e92301f3288a358d1238] [formerly 3359aa65c3ead1483b8f51eb5545f72f9fc80576 [formerly 1cfac92b3e227387f28e959509019d95e5b1b8ec]]
    Former-commit-id: f84ed6ff72e409e8e0c3cc7060ab828c4d789f27 [formerly 85f157643135c67876998e4b4dc667950bf2b331]
    Former-commit-id: 244a7cb3e9b7ed05e5483955614c3ea78f8e7173

--- lights_out/LightsOut.elm
+++ lights_out/LightsOut.elm
@@ -1,14 +1,25 @@
 -- for index query
 -- http://programmers.stackexchange.com/a/131705/50321
-import Graphics.Collage exposing (Form, collage, square, filled, move, toForm)
-import Graphics.Element exposing (Element, show)
-import Color exposing (rgb)
-import List exposing (map, map2, repeat, concat, (::))
+import Graphics.Collage exposing (Form, collage, rect, filled, move, toForm)
+import Graphics.Element exposing (Element, show, below, container, middle)
+import Graphics.Input exposing (button)
+import Color exposing (rgb, grayscale)
+import List exposing (map, map2, repeat, concat, (::), member)
 import Signal
 import Mouse
 import Time
+import Window
 
-type Input = MouseMove (Int, Int) | TimeDelta Float | MouseClick (Int, Int)
+gridSize = 5
+padding = 2
+
+tupOp : (a -> b -> Int) -> (a, a) -> (b, b) -> (Int, Int)
+tupOp op (x, y) (i, j) = (op x i, op y j)
+
+type Input = MouseMove (Int, Int) | TimeDelta Float | MouseClick (Int, Int) | Reset | NoOp
+
+resetButton : Signal.Mailbox Input
+resetButton = Signal.mailbox NoOp
 
 type alias Point =
   { x: Int
@@ -33,30 +44,45 @@ type alias Model =
 initialModel : Model
 initialModel =
   let
-    xs = concat <| map (repeat 5) [0..4]
-    ys = concat <| repeat 5 [0..4]
+    range = [0..(gridSize - 1)]
+    xs = concat <| map (repeat gridSize) range
+    ys = concat <| repeat gridSize range
   in
     { points = map2 p xs ys }
 
-paintSquare : Point -> Form
-paintSquare point =
-  square 50 |>
-    filled
-      (if point.clicked then
-        (rgb 255 (round (50 * point.highlight)) (round (50 * point.highlight)))
-      else
-        (rgb (round (180 * (1 - point.highlight))) (round (180 * (1 - point.highlight))) (round (180 * (1 - point.highlight))))) |>
-    move (toFloat point.x * 52, toFloat point.y * 52)
+paintSquare : (Int, Int) -> Point -> Form
+paintSquare (boardW, boardH) point =
+  let
+    (r, g, b) = (255, round (30 * point.highlight), round (30 * point.highlight))
+    (rectW, rectH) = (boardW//gridSize - padding*2, boardH//gridSize - padding*2)
+    (mx, my) =
+      (point.x, point.y)
+        |> tupOp (*) (rectW, rectH)
+        |> tupOp (-) (boardW//2, boardH//2)
+        |> tupOp (-) (rectW//2, rectH//2)
+        |> tupOp (+) (padding, padding)
+  in
+    rect (toFloat rectW - padding) (toFloat rectH - padding)
+      |> filled (if point.clicked then rgb r g b else grayscale (point.highlight * (-0.05) + 0.6))
+      |> move (toFloat mx, toFloat my)
 
 main : Signal Element
 main =
-  Signal.map
-    (\model ->
-      map paintSquare model.points
-        -- |> (::) (show model.input |> toForm |> move (-20, -100))
-        |> collage 500 500
-    )
+  Signal.map2
+    view
+    Window.dimensions
     currentModel
+
+view : (Int, Int) -> Model -> Element
+view (w, h) model =
+  let
+    (boardW, boardH) = (round ((toFloat w)*0.8 - padding*2), round ((toFloat h)*0.8 - padding*2))
+    btn = (button (Signal.message resetButton.address Reset) "reset")
+  in
+    map (paintSquare (boardW, boardH)) model.points
+      |> collage boardW (boardH - 60)
+      |> container w (h - 60) middle
+      |> below (container w 60 middle btn)
 
 currentModel : Signal Model
 currentModel =
@@ -67,37 +93,38 @@ input =
   Signal.mergeMany [
     (Signal.map MouseMove Mouse.position),
     (Signal.map MouseClick (Signal.sampleOn Mouse.clicks Mouse.position)),
-    (Signal.map TimeDelta (Time.fps 30))
+    (Signal.map TimeDelta (Time.fps 30)),
+    resetButton.signal
   ]
 
 update : Input -> Model -> Model
 update input model =
   case input of
     MouseMove pos ->
-      let
-        (x, y) = correctMousePosition pos
-        points =
-          map
-          (\p -> { p | hover = p.x == x && p.y == y })
-          model.points
-      in
-        { model | points = points }
+      { model | points = map (\p -> { p | hover = (p.x, p.y) == (correctMousePosition pos) }) model.points }
+
     MouseClick pos ->
       let
         (x, y) = correctMousePosition pos
+        affected = map (\(cx, cy) -> (cx + x, cy + y)) [(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)]
         points =
           map
-          (\p -> if p.x == x && p.y == y then { p | clicked = not p.clicked } else p)
+          (\p -> if member (p.x, p.y) affected then { p | clicked = not p.clicked } else p)
           model.points
       in
         { model | points = points }
+
     TimeDelta dt ->
       { model |
         points =
           map
-          (\p -> { p | highlight = if p.hover then 1 else max (p.highlight - 0.2) 0 })
+          (\p -> { p | highlight = if p.hover then 1 else max (p.highlight - 0.08) 0 })
           model.points
       }
+
+    Reset -> initialModel
+
+    NoOp -> model
 
 correctMousePosition : (Int, Int) -> (Int, Int)
 correctMousePosition (x, y) =
