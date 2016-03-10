a627cafcd93e4784b7eb5d5c14f24aebb7346932
Danilo inácio <danilo.isr@gmail.com>
2016-03-10 11:59:18 +0000

    improve rendering quality
    Former-commit-id: 89bbe53b0458947a6511938165c5ff57c526750f
    Former-commit-id: ba9143a955967507554fbd77eecb40ee8e568158 [formerly bdd800e3c9fd2b16079486c01a94784abfa79c4a] [formerly 4df45a66a1af92ddf5ad3fa4eb001c7c42fded50 [formerly 2f2262471b43e5978650c5087e1daf60504f1b5e]] [formerly 7492a8bf1e23b5823b0fa4c1bbe63f9b1c7457ec] [formerly 4ebc33a816bea3d95a9491c4817c1f27a97c4ae9] [formerly a24a972099227157f25fcc6f9ee4b3a1d0679390] [formerly 790c231667c486c94461d7e25ba4eac21e22175a] [formerly 1360489139c45869fde982ce35f0efedfaad32a6 [formerly 1b286ea2b41a1f2d64f10fed4fe3aa62dde8cc05] [formerly 08be28d75ee1052434e7a7e3af48e085f872de63] [formerly d461102a0c95ce513c3b800edae2521f57fe3bed] [formerly a73cbdafd807f7dc6881597effea653fb489d491] [formerly 423717a38ce1ccf0e573c71e5302f884581ca060] [formerly 6fad8199e0bb1689e7f26abb8f29708e2762d701] [formerly a936bb6e67a557245af73c2e63155a66c7383648] [formerly caca21747d688528fe1e53a1748b52998339aa50] [formerly f196b89ab26f9d0be9a68d88d81c5e00f7e2e80d] [formerly 77ac041c65bfa8d09ca5dfc18f73718a34904b52 [formerly 7b8675100eef9f741ad7750dac76d7ade622b6da]]]
    Former-commit-id: ba9143a955967507554fbd77eecb40ee8e568158 [formerly bdd800e3c9fd2b16079486c01a94784abfa79c4a] [formerly 4df45a66a1af92ddf5ad3fa4eb001c7c42fded50 [formerly 2f2262471b43e5978650c5087e1daf60504f1b5e]] [formerly 7492a8bf1e23b5823b0fa4c1bbe63f9b1c7457ec] [formerly 4ebc33a816bea3d95a9491c4817c1f27a97c4ae9] [formerly a24a972099227157f25fcc6f9ee4b3a1d0679390] [formerly 790c231667c486c94461d7e25ba4eac21e22175a]
    Former-commit-id: 1bf6fac3078ad4600b90cb2821ab09e83e7e2f57
    Former-commit-id: 1701d3ceb287a950e8ab7c9f5b4ab5fe3c9b2620
    Former-commit-id: 61641f2c04972d54bb86610bc81596d8921fae65
    Former-commit-id: 9e0cf60448f1fb3d9ea63c5989c4fc0f80f6f3cc
    Former-commit-id: 402d05a3deded65cb5373bb07f4adcd9db9ef64a
    Former-commit-id: f0de5a221577051b27ceb9b792f1eedc860c9854
    Former-commit-id: 06b9f99e0609ec6d3a9f3ff9a97a67f7cae9e422
    Former-commit-id: 1d90523b2b4b53ae92874d175c95276c27bb68db
    Former-commit-id: 0f7a5866e0ae6ac7688492489b58496aaf114c2f
    Former-commit-id: 4e57f3324bda7c28bceef6da17bc65e863922a38 [formerly d6b55d8bc2ba7a241bae9a0cf5fba2c2465b595a]
    Former-commit-id: d328c2864b2a162d012026ea3b5a699163723074
    Former-commit-id: 6df234f4b6de4e023e2f0a6b8c4d1d6eb2f059d6
    Former-commit-id: 0cd0614f3863aa95fa466b0b9a1de08a8716a5fe
    Former-commit-id: c8dca79e3477ed62e47c2b2bd5d0a8f898380974
    Former-commit-id: 8d6d894bb8bb3e50a406383ffc928f4eec595bb9
    Former-commit-id: 8e0115c55253ca67d2bbc78539d86f7e7c60d88c
    Former-commit-id: 5b0b3da83564737c9ef38aa53c067a2fd8464639
    Former-commit-id: 4058b4a0a42978e71c854a061bafbe37aa1166e9
    Former-commit-id: 2e7d8f7b6c53df4d47a006eeb3c9781b8325d691
    Former-commit-id: 84aad68621317b688b461072fd108b586cb1cab8 [formerly d8a7292d8ee67b61e633c9ebe81be094634a6ec6] [formerly 7c3c455e1fb315041eb99446acdcd44e39eda012 [formerly cabce1089b7f24027705d04379fed72c77745b07]] [formerly b7678b80f9506938c19616f67381bf4ad934af08 [formerly 779ac3d44b02449f2e517433cac9ab843983b5d2] [formerly 741ce450fa1735d588f9260ca6fb6fede3a13ae8 [formerly 9a7f5f85bbc4207367bbf44aa471a057886d9f52]]]
    Former-commit-id: 5ce1b12bbbee6a1a0207f4fdaef9ffbe526ef3a7 [formerly 6890234ba45b99adfbcdb05af86f377ed86c2b0d] [formerly 8107c065a3501e864e81b4720ea4dcaca6e7659c [formerly 5adc48a59187a4433f27c10a74085160b2a452bc]]
    Former-commit-id: 52a830ccf61b0deb8d6e5924c323f89caa0911ec [formerly 15f38ac62b73862a48e2f67e297bdfe870cf77a4]
    Former-commit-id: e1b311d8482da015217d3bfcbd0e1724335b3575

--- scratchapixel/raytracer/script.js
+++ scratchapixel/raytracer/script.js
@@ -1,22 +1,19 @@
 // http://www.scratchapixel.com/code.php?id=3&origin=/lessons/3d-basic-rendering/introduction-to-ray-tracing
 +function() {
 
-var sqrt = Math.sqrt;
-var pow = Math.pow;
-
 function Vec3(x, y, z) {
   this.x = x;
   this.y = y;
   this.z = z;
 }
 
-Vec3.prototype.length = function length() {
+Vec3.prototype.length2 = function length2() {
   return this.x * this.x + this.y + this.y + this.z * this.z;
 }
 
 
-Vec3.prototype.length2 = function length2() {
-  return Math.sqrt(this.length());
+Vec3.prototype.length = function length() {
+  return Math.sqrt(this.length2());
 }
 
 Vec3.prototype.dot = function dot(v) {
@@ -26,7 +23,7 @@ Vec3.prototype.dot = function dot(v) {
 Vec3.prototype.normalize = function normalize() {
   var len = this.length2();
   if (len > 0) {
-    var invLen = 1 / len;
+    var invLen = 1 / Math.sqrt(len);
     this.x *= invLen, this.y *= invLen, this.z *= invLen;
   }
 
@@ -41,7 +38,7 @@ Vec3.prototype.cross = function cross(v) {
 }
 
 Vec3.prototype.sum = function sum(v) {
-  return new Vec3(this.x + v.x, + this.y + v.y, + this.z + v.z);
+  return new Vec3(this.x + v.x, this.y + v.y, this.z + v.z);
 }
 
 Vec3.prototype.neg = function neg(v) {
@@ -55,23 +52,22 @@ Vec3.prototype.sub = function sub(v) {
 Vec3.prototype.mult = function mult() {
   if (typeof arguments[0] == 'number') {
     var r = arguments[0];
-    return new Vec3(this.x * r + this.y * r + this.z * r);
+    return new Vec3(this.x * r, this.y * r, this.z * r);
   }
   else {
     var v = arguments[0];
-    return new Vec3(this.x * v.x + this.y * v.y + this.z * v.z);
+    return new Vec3(this.x * v.x, this.y * v.y, this.z * v.z);
   }
 }
 
 
 
-
-function Sphere(center, radius, surfaceColor, emissionColor, transparency, reflection) {
+function Sphere(center, radius, surfaceColor, reflection, transparency, emissionColor) {
   this.center = center;
   this.radius = radius;
   this.radius2 = radius * radius;
   this.surfaceColor = surfaceColor;
-  this.emissionColor = emissionColor;
+  this.emissionColor = emissionColor ? emissionColor : new Vec3(0, 0, 0);
   this.transparency = transparency;
   this.reflection = reflection;
 }
@@ -86,17 +82,6 @@ Sphere.prototype.intersect = function intersect(rayorig, raydir) {
 
   var thc = sqrt(this.radius2 - d2);
   return [true, tca - thc, tca + thc];
-
-  //     float tca = l.dot(raydir);
-  //     if (tca < 0) return false;
-  //     float d2 = l.dot(l) - tca * tca;
-  //     if (d2 > radius2) return false;
-  //     float thc = sqrt(radius2 - d2);
-  //     t0 = tca - thc;
-  //     t1 = tca + thc;
-
-  //     return true;
-
 }
 
 
@@ -105,6 +90,9 @@ Sphere.prototype.intersect = function intersect(rayorig, raydir) {
 var MAX_RAY_DEPTH = 5;
 var INFINITY = 10e8;
 var M_PI = 3.141592653589793
+var sqrt = Math.sqrt;
+var pow = Math.pow;
+
 
 function mix(a, b, mix) {
   return b * mix + a * (1 - mix);
@@ -130,7 +118,7 @@ function trace(rayorig, raydir, spheres, depth) {
   }
 
   // if there's no intersection return black or background color
-  if (sphere == false) return new Vec3(2, 2, 2);
+  if (sphere == false) return new Vec3(1, 1, 1);
 
   var surfaceColor = new Vec3(0, 0, 0);
   var phit = rayorig.sum(raydir.mult(tnear));
@@ -146,20 +134,21 @@ function trace(rayorig, raydir, spheres, depth) {
     nhit = nhit.neg();
     inside = true;
   }
+
   if ((sphere.transparency > 0 || sphere.reflection > 0) && depth < MAX_RAY_DEPTH) {
     var facingratio = raydir.neg().dot(nhit);
     // change the mix value to tweak the effect
     var fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
     // compute reflection direction (not need to normalize because all vectors
     // are already normalized)
-    var refldir = raydir.sub(nhit).mult(2).mult(raydir.dot(nhit));
+    var refldir = raydir.mult(raydir.dot(nhit)).mult(2).sub(nhit);
     refldir.normalize();
     var reflection = trace(phit.sum(nhit.mult(bias)), refldir, spheres, depth + 1);
-    var refraction = 0;
+    var refraction = new Vec3(0, 0, 0);
     // if the sphere is also transparent compute refraction ray (transmission)
     if (sphere.transparency) {
       var ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
-      var cosi = nhit.neg().dot(raydir);
+      var cosi = -nhit.dot(raydir);
       var k = 1 - eta * eta * (1 - cosi * cosi);
       var refrdir = raydir.mult(eta).sum(nhit.mult(eta *  cosi - sqrt(k)));
       refrdir.normalize();
@@ -167,22 +156,25 @@ function trace(rayorig, raydir, spheres, depth) {
     }
     // the result is a mix of reflection and refraction (if the sphere is transparent)
     surfaceColor = sphere.surfaceColor.mult(
-      reflection * fresneleffect + refraction * (1- fresneleffect) * sphere.transparency);
+      reflection.mult(fresneleffect).sum(
+        refraction.mult((1 - fresneleffect) * sphere.transparency)
+      )
+    );
+
   }
   else {
     // it's a diffuse object, no need to raytrace any further
     for (var i in spheres) {
       if (spheres[i].emissionColor.x > 0) {
         // this is a light
-        var transmission = 1;
+        var transmission = new Vec3(1, 1, 1);
         var lightDirection = spheres[i].center.sub(phit);
         lightDirection.normalize();
         for (var j in spheres) {
           if (i != j) {
             var intersection = spheres[j].intersect(phit.sum(nhit.mult(bias)), lightDirection);
             if(intersection[0]) {
-              t0 = intersection[1], t1 = intersection[2];
-              transmission = 0;
+              transmission = new Vec3(0, 0, 0);
               break;
             }
           }
@@ -191,7 +183,7 @@ function trace(rayorig, raydir, spheres, depth) {
           sphere.surfaceColor
           .mult(transmission)
           .mult(Math.max(0, nhit.dot(lightDirection)))
-          .mult(sphere[i].emissionColor)
+          .mult(spheres[i].emissionColor)
         );
       }
     }
@@ -202,15 +194,14 @@ function trace(rayorig, raydir, spheres, depth) {
 
 
 
-function render(spheres) {
-    var width = 640, height = 480;
+function render(spheres, w, h) {
     var pixels = [];
-    var invWidth = 1 / width, invHeight = 1 / height;
-    var fov = 30, aspectratio = width / height;
-    var angle = Math.tan(M_PI * 0.5 * fov / 180.);
+    var invWidth = 1 / w, invHeight = 1 / h;
+    var fov = 30, aspectratio = w / h;
+    var angle = Math.tan(M_PI * 0.5 * fov / 180);
     // Trace rays
-    for (var y = 0; y < height; ++y) {
-        for (var x = 0; x < width; ++x) {
+    for (var y = 0; y < h; ++y) {
+        for (var x = 0; x < w; ++x) {
             var xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
             var yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
             var raydir = new Vec3(xx, yy, -1);
@@ -218,7 +209,6 @@ function render(spheres) {
             pixels.push(trace(new Vec3(0, 0, 0), raydir, spheres, 0));
         }
     }
-    // Save result to a PPM image (keep these flags if you compile under Windows)
     return pixels;
 }
 
@@ -232,6 +222,26 @@ spheres.push(new Sphere(new Vec3( 5.0,      0, -25),     3, new Vec3(0.65, 0.77,
 spheres.push(new Sphere(new Vec3(-5.5,      0, -15),     3, new Vec3(0.90, 0.90, 0.90), 1, 0.0));
 // light
 spheres.push(new Sphere(new Vec3( 0.0,     20, -30),     3, new Vec3(0.00, 0.00, 0.00), 0, 0.0, new Vec3(3, 3, 3)));
-window.pixels = render(spheres);
+
+var w = 340, h = 280;
+var context2d = document.getElementById('canvas').getContext('2d');
+var imageData = context2d.getImageData(0, 0, w, h);
+var rawData = imageData.data;
+var pixel, index = 0;
+
+var pixels = render(spheres, w, h);
+
+for (var i in pixels) {
+  pixel = pixels[i];
+
+  rawData[index++] = ~~(pixel.x * 255);
+  rawData[index++] = ~~(pixel.y * 255);
+  rawData[index++] = ~~(pixel.z * 255);
+  rawData[index++] = 255;
+}
+
+context2d.putImageData(imageData, 0, 0);
+
+window.pixels = pixels;
 
 }();
