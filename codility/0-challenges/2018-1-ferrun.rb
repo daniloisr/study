848266703d4de6f689a104fa991c0e53e1a74fb4
Danilo Resende <danilo.isr@gmail.com>
2018-03-25 11:09:01 -0300

    [codility] Final version 2018 ferrun
    Used prefix sum logic to calculate the highest sum

--- codility/0-challenges/2018-1-ferrun.rb
+++ codility/0-challenges/2018-1-ferrun.rb
@@ -1,57 +1,17 @@
 require 'byebug'
 require 'minitest/autorun'
 
-Slice = Struct.new(:sum, :count) do
-  include Comparable
-
-  def self.zero; new(0, 0); end
-
-  def +(b)
-    self.class.new(sum + b.sum, count + b.count)
-  end
-
-  def <=>(b)
-    sum <=> b.sum
-  end
-
-  def nonnegative
-    self >= self.class.zero
-  end
-
-  def inspect
-    "(#{sum},#{count})"
-  end
-end
-
-def z; Slice.zero; end
-
 def solution(xs)
-  shrink(xs.map { |x| Slice.new(x, 1) }, 0)
-end
+  prefix = {}
+  sum = 0
 
-def sum(xs)
-  sum, memo = xs.reduce([z, []]) do |(sum, memo), x|
-    sum = sum + x
-    sum > z ?
-      [sum, memo] :
-      [z,   memo + [sum]]
-  end
+  xs.each_with_index.reduce(0) do |slice, (x, i)|
+    sum += x
 
-  memo + (sum > z ? [sum] : [])
-end
-
-def shrink(xs, i, shrank = false)
-  xs = sum(xs)
-  a, b = xs[i], xs[i+1]
-
-  p [i, shrank, xs]
-
-  case
-  when b.nil? && shrank then shrink(xs, 0)
-  when b.nil?           then xs.lazy.select(&:nonnegative).map(&:count).max || z
-  when a + b > z        then shrink(xs[0...i] + [a + b] + xs[i+2..-1], i > 0 ? i - 1 : i, true)
-  when a + b == z       then [shrink(xs[0...i] + [a + b] + xs[i+2..-1], i, true), shrink(xs, i + 1, shrank)].max
-  else                       shrink(xs, i + 1, shrank)
+    next i + 1 if sum >= 0
+    next [slice, i - prefix[sum]].max if prefix.key?(sum)
+    prefix[sum] = i
+    slice
   end
 end
 
@@ -62,6 +22,11 @@ class TestSolution < Minitest::Test
 
   def test_sample2
     assert_equal 4, solution([1, 1, -1, -1, -1, -1, -1, 1, 1])
+  end
+
+  def test_sample3
+    assert_equal 5, solution([0, -1, 0, 1, 0, -1])
+    # assert_equal 6, solution([0, -1, 0, 0, 1, 0, -1, -1])
   end
 
   def test_sample_abc
@@ -78,6 +43,13 @@ class TestSolution < Minitest::Test
 
   def test_custom2
     assert_equal 2, solution([1, -1])
+  end
+
+  def test_custom3
+    assert_equal 3, solution([1, -1, 0])
+    assert_equal 2, solution([1, -1, -1])
+    assert_equal 1, solution([0, -1, -1])
+    assert_equal 0, solution([-1, -1, -1])
   end
 
   def test_tricky1
